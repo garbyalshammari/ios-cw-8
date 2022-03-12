32e36140b00123a9495d03c4bd159e9b6b3d4491
غربي الشمري <100927646+garbyalshammari@users.noreply.github.com>
2022-03-12 11:19:19 +0300

    yfdu

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -2,4 +2,9 @@ import UIKit
 
 // التمرين الأول
 // ClassWork 1
-
+let name = "gharbi"
+let birthday = 2005
+var age = 16
+var curyear = 2022
+curyear - birthday
+print("hello my name is\(name)i was born in\(birthday)")
