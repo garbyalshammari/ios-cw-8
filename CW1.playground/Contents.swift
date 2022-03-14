49c3c422f3d69757c3c6fad09e66f0c22815a831
غربي الشمري <100927646+garbyalshammari@users.noreply.github.com>
2022-03-14 15:54:55 +0300

    yrytr

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -8,3 +8,7 @@ var age = 16
 var curyear = 2022
 curyear - birthday
 print("hello my name is\(name)i was born in\(birthday)")
+var num1="8"
+var num2 = 4
+var num3 = 2.5
+num3 = Double(num2) + (Double(num1) ?? 8)
